188e7118387543bc8c37fbedb48c59624e981eac
Johnny9874 <ye.johnnypro@gmail.com>
2025-01-25 00:48:00 +0100

    dernier changement

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'monorganisation/todo-list',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6d503d149ba8ce55720c18b1a402cb336c036af4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monorganisation/todo-list' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6d503d149ba8ce55720c18b1a402cb336c036af4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
