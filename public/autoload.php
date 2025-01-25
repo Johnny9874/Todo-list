84d6f937eae035cb81a9b36240f04f0445d8a9b9
Johnny9874 <ye.johnnypro@gmail.com>
2025-01-25 01:44:22 +0100

    mise a jour

--- public/autoload.php
+++ public/autoload.php
@@ -1,5 +1,4 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include __DIR__ . '/src/' . str_replace('\\', '/', $class_name) . '.php';
+    include __DIR__ . '/../src/controllers/' . str_replace('\\', '/', $class_name) . '.php';
 });
-?>
