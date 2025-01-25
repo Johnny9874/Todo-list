e53104f594910747d97d4951d17f3029a356d671
Johnny9874 <ye.johnnypro@gmail.com>
2025-01-25 01:33:23 +0100

    correction chemin

--- public/index.php
+++ public/index.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Inclure les contrôleurs nécessaires
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/autoload.php';
+
 
 // Vérifiez l'action de la requête
 if (isset($_GET['action'])) {
