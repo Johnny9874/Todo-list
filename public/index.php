ec93a07924038b86218224af36c9e86459012be3
Johnny9874 <ye.johnnypro@gmail.com>
2025-01-25 01:30:22 +0100

    modif index.php

--- public/index.php
+++ public/index.php
@@ -8,34 +8,30 @@ require_once __DIR__ . '/../autoload.php';
 
 // Vérifiez l'action de la requête
 if (isset($_GET['action'])) {
+    // Créer une instance du contrôleur une seule fois
     $controller = new UserController();
-
-    // Si l'action est "register", on appelle la méthode register
-    if ($_GET['action'] === 'register') {
-        $controller->register();
-    }
-    // Si l'action est "login", on appelle la méthode login
-    elseif ($_GET['action'] === 'login') {
-        $controller->login();
-    }
-}
-
-if ($_GET['action'] === 'profile') {
-    $controller = new UserController();
-    $user = $controller->getProfile();
-
-    if ($user) {
-        include __DIR__ . '/html/profile.html';
-    } else {
-        echo "Utilisateur non trouvé";
-    }
-}
-
-if (isset($_GET['action'])) {
-    $action = $_GET['action'];
     
-    if ($action === 'updateProfile') {
-        $controller = new UserController();
-        $controller->updateProfile();
+    // Gérer les différentes actions
+    switch ($_GET['action']) {
+        case 'register':
+            $controller->register();
+            break;
+        case 'login':
+            $controller->login();
+            break;
+        case 'profile':
+            $user = $controller->getProfile();
+            if ($user) {
+                include __DIR__ . '/html/profile.html';
+            } else {
+                echo "Utilisateur non trouvé";
+            }
+            break;
+        case 'updateProfile':
+            $controller->updateProfile();
+            break;
+        default:
+            echo "Action non reconnue";
+            break;
     }
 }
