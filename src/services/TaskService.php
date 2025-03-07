51f513e5f9e5021c789f80181e0079cd5b49598d
Johnny9874 <ye.johnnypro@gmail.com>
2025-03-08 00:07:58 +0100

    modif chemin 33

--- src/services/TaskService.php
+++ src/services/TaskService.php
@@ -14,12 +14,12 @@ class TaskService {
     public function addTask($title, $description, $userId, $priority, $status, $due_date, $task_data) {
         global $conn;
     
-        // Vérifier si l'utilisateur est connecté
+        // Vérifie si l'utilisateur est connecté
         if (!isset($_SESSION['user_id'])) {
             throw new Exception("Utilisateur non connecté.");
         }
     
-        // Récupérer l'ID de l'utilisateur connecté depuis la session
+        // Récupère l'ID de l'utilisateur connecté depuis la session
         $userId = $_SESSION['user_id'];
     
         // Vérifier si l'utilisateur existe
@@ -35,7 +35,7 @@ class TaskService {
     
         // Encoder correctement les données task_data en JSON
         $encoded_task_data = json_encode($task_data);
-    
+        
         // Vérifier si l'encodage JSON a réussi
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new Exception('Erreur lors de l\'encodage des données en JSON: ' . json_last_error_msg());
@@ -57,18 +57,14 @@ class TaskService {
             die("Error executing the query: " . $stmt->error);
         }
     
-        // Récupérer la tâche ajoutée par son ID
-        $taskId = $stmt->insert_id;  // Obtenir l'ID de la tâche insérée
-        $task = $this->taskDAO->getTaskById($taskId);  // Utiliser TaskDAO pour récupérer la tâche
-    
-        if ($task === null) {
-            throw new Exception("La tâche ajoutée n'a pas pu être récupérée.");
-        }
-    
+        // Récupérer l'ID de la tâche insérée
+        $taskId = $stmt->insert_id;
         $stmt->close();
         
-        return $task; // Renvoyer la tâche ajoutée
+        // Récupérer la tâche ajoutée
+        return $this->getTaskById($taskId);  // Retourner la tâche ajoutée
     }
+    
     
 
     // Récupérer les tâches de MySQL
